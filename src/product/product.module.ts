55e65a9bc8521a8f4ffd67337b377e123e7cebc9
sschrs <s_ozarslan07@outlook.com>
2023-03-17 02:47:12 +0300

    providers and controllers were added to product module

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,9 +1,13 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { ProductController } from "./product.controller";
 import { Product } from "./product.entitiy";
+import { ProductService } from "./product.service";
 
 @Module({
     imports: [ TypeOrmModule.forFeature([Product]) ],
-    exports: [TypeOrmModule]
+    exports: [TypeOrmModule],
+    controllers: [ProductController],
+    providers: [ProductService]
 })
 export class ProductModule{}
