8a36ba05534cce8e932b2b1854aa80c2578519ae
sschrs <s_ozarslan07@outlook.com>
2023-03-16 21:45:49 +0300

    typeorm and mysql installed & configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
   imports: [
-    ConfigModule.forRoot()
+    ConfigModule.forRoot(), // get env variables from .env file
+    // database configuration
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: (process.env.DATABASE_PORT as unknown as number),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true
+    })
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  //inject data source
+  constructor(private dataSource: DataSource){}
+}
