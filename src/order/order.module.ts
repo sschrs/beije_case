670907904c8cc59466fb79fa46b0d75b2b1ed4d9
sschrs <s_ozarslan07@outlook.com>
2023-03-17 02:46:06 +0300

    providers and controllers were added to module

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,10 +1,14 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { OrderDetail } from "./order-detail.entity";
+import { OrderController } from "./order.controller";
 import { Order } from "./order.entity";
+import { OrderService } from "./order.service";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Order, OrderDetail])],
-    exports: [TypeOrmModule]
+    exports: [TypeOrmModule],
+    providers: [OrderService],
+    controllers: [OrderController]
 })
 export class OrderModule{}
