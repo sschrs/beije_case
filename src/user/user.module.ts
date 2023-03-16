054e5a56f834df82c035adde483dc7ab6d3ed96e
sschrs <s_ozarslan07@outlook.com>
2023-03-16 23:41:44 +0300

    providers and controllers are added to module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,13 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserController } from "./user.controller";
 import { User } from "./user.entity";
+import { UserService } from "./user.service";
 
 @Module({
     imports: [TypeOrmModule.forFeature([User])],
-    exports: [TypeOrmModule]
+    exports: [TypeOrmModule],
+    providers: [UserService],
+    controllers: [UserController]
 })
 export class UserModule { }
