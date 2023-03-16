21d9cfa4b742d8369ca277e9984eba196dbb73b1
sschrs <s_ozarslan07@outlook.com>
2023-03-17 00:12:39 +0300

    providers and controllers were added to module

--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -1,9 +1,13 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { AddressController } from "./address.controller";
 import { Address } from "./address.entity";
+import { AddressService } from "./address.service";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Address])],
-    exports: [TypeOrmModule]
+    exports: [TypeOrmModule],
+    providers: [AddressService],
+    controllers: [AddressController]
 })
 export class AddressModule{}
