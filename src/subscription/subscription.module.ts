fdc4faea6b51987e44e23a15c28d154edb5844ec
sschrs <s_ozarslan07@outlook.com>
2023-03-17 15:01:47 +0300

    providers and controllers are added to subscription module

--- src/subscription/subscription.module.ts
+++ src/subscription/subscription.module.ts
@@ -1,10 +1,14 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { SubscriptionDetail } from "./subscription-detail.entity";
+import { SubscriptionController } from "./subscription.controller";
 import { Subscription } from "./subscription.entity";
+import { SubscriptionService } from "./subscription.service";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Subscription, SubscriptionDetail])],
-    exports: [TypeOrmModule]
+    exports: [TypeOrmModule],
+    providers: [SubscriptionService],
+    controllers: [SubscriptionController]
 })
 export class SubscriptionModule { }
